94b89047b1f35fc77c64b3987651472cb5f26577
Isi <isidro.pq@hotmail.com>
2025-05-31 03:59:49 +0800

    Delete contracts directory

--- contracts/DBSTEToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-contract DBSTEToken is ERC20, Ownable {
-    constructor() ERC20("Decentralised Bike-Share Token", "DBSTE") {}
-    function mint(address to, uint256 amount) external onlyOwner { _mint(to, amount); }
-}
